6f6768e7f917ac34cc8666e45a80945db74f6ae3
DangAnhDuc <daduc@fossil.com>
2021-12-31 12:40:14 +0700

    Update date format

--- app/src/main/java/com/example/weatherapp/common/Constants.kt
+++ app/src/main/java/com/example/weatherapp/common/Constants.kt
@@ -21,7 +21,7 @@ class Constants {
         const val WEATHER_TABLE = "weather_table"
 
         //Time
-        const val DATE_FORMAT_PATTERN = "EEE, MMM d yyyy"
+        const val DATE_FORMAT_PATTERN = "EEE, d MMM yyyy"
         const val ONE_SECOND_IN_MILLIS = 1000L
         const val ONE_MINUTE_IN_SECONDS = 60
         const val ONE_HOUR_IN_SECONDS = 60
